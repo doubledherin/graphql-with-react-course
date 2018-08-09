37cbf9ed5e209deb34f3fe0d85e696ee7c4d37eb
Wendy Dherin <wendydherin@gmail.com>
2018-08-09 09:53:54 +0100

    Handle Signup mutation.

---
+++ auth-graphql-starter/client/components/SignupForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import AuthForm from './AuthForm'
+import mutation from '../mutations/Signup'
+import query from '../queries/CurrentUser'
+
+class SignupForm extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      errors: []
+    }
+  }
+
+  handleSignup({ email, password}) {
+    this.props.mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    }).catch(res => {
+      const errors = res.graphQLErrors.map(error => error.message)
+      this.setState({ errors })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Sign up</h3>
+        <AuthForm
+          onSubmit={ this.handleSignup.bind(this) }
+          errors={this.state.errors}/>
+      </div>
+    )
+  }
+}
+
+export default graphql(mutation)(SignupForm)
